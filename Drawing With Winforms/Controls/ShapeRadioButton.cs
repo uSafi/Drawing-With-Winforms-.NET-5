7b9a7b31ad094fc80b666bd86cd4f50cbbb9a65f
Usama.S.Y <43465134+uSafi@users.noreply.github.com>
2022-06-20 09:46:33 +0300

    Add project files.

---
+++ Drawing With Winforms/Controls/ShapeRadioButton.cs
@@ -0,0 +1,10 @@
+﻿using Drawing_With_Winforms.Enums;
+using System.Windows.Forms;
+
+namespace Drawing_With_Winforms.Controls
+{
+    internal class ShapeRadioButton : RadioButton
+    {
+        public ShapeType ShapeType { get; set; }
+    }
+}
